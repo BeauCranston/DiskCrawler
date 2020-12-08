6a43e09912fa24da3d38cfee28282e20b27e419b
BeauCranston <beau2tie@gmail.com>
2020-12-07 23:55:51 -0500

    etrsdfg

--- DiskCrawler/src/Controller.java
+++ DiskCrawler/src/Controller.java
@@ -27,18 +27,47 @@ public class Controller {
     @FXML TextArea pathList;
     @FXML Button searchBtn;
 
+    ArrayList<String> paths = new ArrayList<>();
+    ProducerConsumer pc;
+
+    /**
+     * when the search button is clicked start the search task
+     * @param e
+     */
     public void search(MouseEvent e){
-
-
+        System.out.println("search called");
+        Thread t1 = new Thread(searchTask);
+        //run the task in the background
+        t1.setDaemon(true);
+        t1.start();
     }
 
-
+    /**
+     * this task performs the search for the files.
+     */
     Task searchTask = new Task() {
         @Override
-        protected Object call() throws Exception {
+        protected Void call() throws Exception {
+            System.out.println("running task");
             String dirInput = directoryInput.getText();
             String fileInput = targetFile.getText();
-            ProducerConsumer.startIndexing(dirInput, fileInput);
+            pc = new ProducerConsumer();
+            pc.startIndexing(dirInput, fileInput);
+            Platform.runLater(updateFileFound);
+            return null;
         }
-    }
+    };
+
+    /**
+     * this task updates the ui with the data received from the producer consumer
+     */
+    Task updateFileFound = new Task() {
+        @Override
+        protected Void call() throws Exception {
+            int count  = pc.getMatchcount();
+            filesFound.setText(String.valueOf(count));
+            pathList.setText(pc.getMatches());
+            return null;
+        }
+    };
 }
--- DiskCrawler/src/ProducerConsumer.java
+++ DiskCrawler/src/ProducerConsumer.java
@@ -1,4 +1,6 @@
 
+import javafx.collections.ObservableList;
+
 import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
@@ -11,7 +13,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  * <p/>
  * Producer and consumer tasks in a desktop search application
  *
- * @author Brian Goetz and Tim Peierls @betterAuthor Beau Cranston 000397019
+ * @author Brian Goetz and Tim Peierls
+ *
+ * @betterAuthor Beau Cranston 000397019 , certify that this material is my
+ *  * original work. No other person's work has been used without due
+ *  * acknowledgement and I have not made my work available to anyone else.
+ *
  */
 public class ProducerConsumer {
     /**
@@ -24,19 +31,28 @@ public class ProducerConsumer {
     private static AtomicInteger currentCrawlCount = new AtomicInteger();
     //A DEBUG TOGGLER NICE! You taught me this and i think it's a smart way of keeping things clean instead of commenting out outputs, so thank you
     private static boolean debug = true;
-    public static <T> void debugWrite(T msg){
-        if(debug == true){
-            System.out.println(msg);
-        }
-
-    }
     private static ConcurrentHashMap<String, File> filesFound = new ConcurrentHashMap<>();
     private static final int BOUND = 10;
+    //the number of consumers to start with
     private static final int N_CONSUMERS = Runtime.getRuntime().availableProcessors();
+    //instantiate thread pools
     private static ExecutorService producerService = Executors.newScheduledThreadPool(N_CONSUMERS);
     private static ExecutorService consumerService = Executors.newScheduledThreadPool(N_CONSUMERS);
+    //string build to return the files paths ot the text area in the GUI
+    public static StringBuilder matches;
+    //volatile int to return to the gui for the files found count
+    public static volatile int matchCount;
+    //lock to make the startIndexing method wait until the consumer has finished indexing
+    private static final Object lock = new Object();
+
+    public ProducerConsumer(){
+        matches = new StringBuilder();
+        matchCount = 0;
+    }
+
 
     static class FileCrawler implements Runnable {
+
         private final BlockingQueue<File> fileQueue;
         private final FileFilter fileFilter;
         private final File root;
@@ -52,7 +68,12 @@ public class ProducerConsumer {
                 }
             };
         }
-    //checks if the file has already been indexed
+
+        /**
+         * checks the global hashmap for the filepath hash to determine if the indexer has already gone over the file
+         * @param f file to check
+         * @return a boolean of whether or not the file has been indexed
+         */
         private boolean alreadyIndexed(File f) {
             if(filesFound.containsKey(f.getAbsolutePath())){
                 return true;
@@ -65,15 +86,14 @@ public class ProducerConsumer {
 
         public void run() {
             try {
-                //crawl now returns true when it is done, when the crawling is done, the done boolean should be true to notify the consumers that they can terminate
+                //increment the current crawl count to set how many producers are currently running
                 currentCrawlCount.getAndIncrement();
+                //crawl on the file
                 crawl(root);
+                //decrement the current crawl count to set how many producers are currently running
                 currentCrawlCount.getAndDecrement();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-            }
-            finally {
-                //producerService.shutdown();
             }
 
         }
@@ -86,11 +106,14 @@ public class ProducerConsumer {
          * @throws InterruptedException
          */
         private boolean crawl(File root) throws InterruptedException {
+            //gets the children files
             File[] entries = root.listFiles(fileFilter);
             if (entries != null) {
                 for (File entry : entries)
                     if (entry.isDirectory())
+                        //fire a new thread for each child in the directory
                         producerService.submit(new FileCrawler(fileQueue ,fileFilter ,entry));
+                    //if the file has not been indexed then get the file path and add it to the concurrenthashmap
                     else if (!alreadyIndexed(entry)){
                         //add file to hashmap
                         //debugWrite(entry.getAbsolutePath());
@@ -104,10 +127,9 @@ public class ProducerConsumer {
 
     static class Indexer implements Runnable {
         private final BlockingQueue<File> queue;
-        //static final object so that all consumer threads have visibility of the lock and the lock cannot be modified
-        private static final Object lock = new Object();
+        //target file being passed fro mthe constructore
         private String targetFile;
-        private ArrayList<String> matches = new ArrayList<>();
+
         private static volatile boolean indexingDone = false;
         public Indexer(BlockingQueue<File> queue, String targetFile) {
             this.queue = queue;
@@ -116,22 +138,26 @@ public class ProducerConsumer {
 
         public void run() {
             try {
-                //only loop while the disk crawler has not finished crawling
+                //while the crawlers are not finished running, keep indexing the files
                 while (currentCrawlCount.get() > 0){
                     synchronized (lock){
+                        //index the file from the queue
                         indexFile(queue.take());
                     }
 
                 }
                 //System.out.println("outta while");
+                //shut down the consumer so that it will finish its remaining tasks
                 consumerService.shutdown();
-                producerService.shutdownNow();
+                //shut down the producer just in case it has remaining tasks
+                producerService.shutdown();
 
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
             //check if the thread is alive after execution, will return true but this is because it is just about to die off. Kind of useless code here.
             finally {
+                //if all else fails then SHUT IT DOWNNNNNNNNN
                 consumerService.shutdownNow();
                 producerService.shutdownNow();
             }
@@ -141,12 +167,16 @@ public class ProducerConsumer {
          * prints the file path and the value of the counter and then increments the static volatile counter
          *
          */
-        public void indexFile(File file) {
+        public synchronized void indexFile(File file) {
             // Index the file...
             //see the file that is being indexed
             debugWrite(file.getPath());
-            if(file.getName().equals(targetFile)){
-                matches.add(file.getAbsolutePath());
+            //check if the target file matches the new file being passed
+            if(targetFile.matches(file.getName())){
+                //if it is a match append the absolute path string to the string builder
+                matches.append(file.getAbsolutePath() + "\n");
+                //increment the match count
+                matchCount++;
                 debugWrite("Found File: " + file.getAbsolutePath());
             }
             //print the value of the counter
@@ -156,8 +186,8 @@ public class ProducerConsumer {
 
     }
 
-    public static void startIndexing(String directory, String fileName) {
-        File[] directories = {new File(directory)};
+    public void startIndexing(String pathName, String fileName) {
+        File[] directories = {new File(pathName)};
         BlockingQueue<File> queue = new LinkedBlockingQueue<File>(BOUND);
         FileFilter filter = new FileFilter() {
             public boolean accept(File file) {
@@ -165,21 +195,65 @@ public class ProducerConsumer {
             }
         };
 
-        for (File root : roots)
+        for (File root : directories)
+            //fire a new task for the root directory so that the file crawler crawls on the root
             producerService.submit(new FileCrawler(queue, filter, root));
 
         for (int i = 0; i < N_CONSUMERS; i++)
+            //fire nconsumer tasks
             consumerService.submit(new Indexer(queue,fileName));
 
+        synchronized (lock){
+            //lock this thread while the consumer service is running
+            while(!consumerService.isShutdown()){
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            //once the consumer service has finished, see what we got
+            System.out.println(getMatches());
+            System.out.println(getMatchcount());
+        }
 
 
     }
+
+    /**
+     * method for out put. only fro debugging purposes
+     * @param msg
+     * @param <T>
+     */
+    public static <T> void debugWrite(T msg){
+        if(debug == true){
+            System.out.println(msg);
+        }
+
+    }
+
+    /**
+     * gets the matches as a string and returns it
+     * @return
+     */
+    public String getMatches(){
+        return matches.toString();
+    }
+
+    /**
+     * gets the match count
+     * @return int
+     */
+    public int getMatchcount(){
+        return matchCount;
+    }
+
 
 //    public static void main(String[] args) {
 //
 //        File[] directories = {new File("C:\\Users\\Beau\\Desktop\\test10183")};
 //        startIndexing(directories, "Hamilton.txt");
-//
+//          C:\Users\Beau\Desktop\test10183
 //        //hold this thread up until the program finishes
 //    }
 }
